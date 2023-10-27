35c96acc251c2b70c14fa01d39e77e0edb2facb2
kthawani <kthawani@entera.ai>
2023-10-27 15:09:20 +0530

    Sign up Form binded with API

--- pages/api/auth/sign_up.js
+++ pages/api/auth/sign_up.js
@@ -2,9 +2,17 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
   const { email, password } = req.body;
 
-  if (!email || !email.includes("@") || !name || password.trim().length < 7) {
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
     res.status(422).json({
       message:
         "Invalid Input - password should also be atleast 7 characters long.",
@@ -14,7 +22,7 @@ async function handler(req, res) {
   const client = await connectToDatabase();
   const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password);
 
   const result = await db.collection("users").insertOne({
     email: email,
